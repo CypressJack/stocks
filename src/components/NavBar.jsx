903804724b4be4049b7fd4e4fe6ea0763697c155
Wade Croft <wadecroft@gmail.com>
2021-06-14 23:05:08 -0700

    added icon assets

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,8 +6,15 @@ import icon2 from "../assets/icons/nav-icon-2-grey.png"
 import icon3 from "../assets/icons/nav-icon-3-grey.png"
 import icon4 from "../assets/icons/nav-icon-4-grey.png"
 import icon5 from "../assets/icons/nav-icon-5-grey.png"
+import icon1black from "../assets/icons/nav-icon-1-black.png"
+import icon2black from "../assets/icons/nav-icon-2-black.png"
+import icon3black from "../assets/icons/nav-icon-3-black.png"
+import icon4black from "../assets/icons/nav-icon-4-black.png"
+import icon5black from "../assets/icons/nav-icon-5-black.png"
 
 export default function Navbar(props) {
+
+
 
   return (
     <div className='nav-bar'>
